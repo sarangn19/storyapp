509712c96db8f250ca2e810dcb17ce04d3229fdd
sarangn19 <sarangn19@gmail.com>
2026-06-06 22:37:26 +0530

    add eatho story

--- src/constants/stories.js
+++ src/constants/stories.js
@@ -3,6 +3,7 @@ const STORIES = [
   { title: 'The Den', file: require('../../assets/stories/DEN_SSFSGU_Final_Edit94zh3.mp3') },
   { title: 'Shararanthal', file: require('../../assets/stories/shararanthal.mp3') },
   { title: 'Oru Chembaneer', file: require('../../assets/stories/oru_chembaneer.mp3') },
+  { title: 'Eatho', file: require('../../assets/stories/eatho.mp3') },
 ];
 
 export default STORIES;
