323f952784f1094d28421f9eca178370e2db64d1
sarangn19 <sarangn19@gmail.com>
2026-06-06 22:36:31 +0530

    add oru chembaneer story

--- src/constants/stories.js
+++ src/constants/stories.js
@@ -2,6 +2,7 @@ const STORIES = [
   { title: 'Clouds', file: require('../../assets/stories/CLOUDS_by_Neil_Bebber_s1.mp3') },
   { title: 'The Den', file: require('../../assets/stories/DEN_SSFSGU_Final_Edit94zh3.mp3') },
   { title: 'Shararanthal', file: require('../../assets/stories/shararanthal.mp3') },
+  { title: 'Oru Chembaneer', file: require('../../assets/stories/oru_chembaneer.mp3') },
 ];
 
 export default STORIES;
