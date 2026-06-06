1828ce5178db63b78b234eba7119bae432c5d181
sarangn19 <sarangn19@gmail.com>
2026-06-06 22:32:22 +0530

    add shararanthal story

--- src/constants/stories.js
+++ src/constants/stories.js
@@ -1,6 +1,7 @@
 const STORIES = [
   { title: 'Clouds', file: require('../../assets/stories/CLOUDS_by_Neil_Bebber_s1.mp3') },
   { title: 'The Den', file: require('../../assets/stories/DEN_SSFSGU_Final_Edit94zh3.mp3') },
+  { title: 'Shararanthal', file: require('../../assets/stories/shararanthal.weba') },
 ];
 
 export default STORIES;
