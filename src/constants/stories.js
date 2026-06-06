66562e1d754af8e1122c0e548b69edbc1e254866
sarangn19 <sarangn19@gmail.com>
2026-06-06 22:35:13 +0530

    fix: rename shararanthal .weba to .mp3 for Metro bundler compat

--- src/constants/stories.js
+++ src/constants/stories.js
@@ -1,7 +1,7 @@
 const STORIES = [
   { title: 'Clouds', file: require('../../assets/stories/CLOUDS_by_Neil_Bebber_s1.mp3') },
   { title: 'The Den', file: require('../../assets/stories/DEN_SSFSGU_Final_Edit94zh3.mp3') },
-  { title: 'Shararanthal', file: require('../../assets/stories/shararanthal.weba') },
+  { title: 'Shararanthal', file: require('../../assets/stories/shararanthal.mp3') },
 ];
 
 export default STORIES;
